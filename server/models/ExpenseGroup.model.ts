36b62bc756fde2d3a76c0f71f50235a1c32df5e3
miacias <miaciasullo@gmail.com>
2025-10-14 21:28:41 -0400

    server typescript fix

--- server/models/ExpenseGroup.model.ts
+++ server/models/ExpenseGroup.model.ts
@@ -1,21 +1,34 @@
 import { Schema, Types, model } from 'mongoose';
 import { expenseItemSchema } from './ExpenseItem.js';
 
-const expenseGroupSchema = new Schema({
+interface ExpenseItem {
+  amount: number;
+  [key: string]: any;
+}
+
+interface ExpenseGroup {
+  name: string;
+  items: ExpenseItem[];
+  userId: Types.ObjectId;
+  total?: number;
+}
+
+const expenseGroupSchema = new Schema<ExpenseGroup>({
   name: { type: String, required: true },
   items: [expenseItemSchema],
-  userId: { type: Types.ObjectId, ref: 'User', required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 }, {
   toJSON: { virtuals: true },
   timestamps: true,
 });
 
-expenseGroupSchema.virtual('total').get(() => {
+expenseGroupSchema.virtual('total').get(function(this: ExpenseGroup) {
   if (!this.items || !this.items.length) return 0;
-  return this.items.reduce((sum, item) => sum + item.amount, 0);
+  return this.items.reduce((sum, item: ExpenseItem) => sum + item.amount, 0);
 })
 
 // compiled model
-const ExpenseGroup = model('ExpenseGroup', expenseGroupSchema);
+const ExpenseGroup = model<ExpenseGroup>('ExpenseGroup', expenseGroupSchema);
 
 export { ExpenseGroup };
+export type { ExpenseGroup as ExpenseGroupType, ExpenseItem as ExpenseItemType };
